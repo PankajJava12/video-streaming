4d20bef19e81f96503c168b1c4dc560c3ceac033
Pankaj Dewangan <pankaj.java12@gmail.com>
2025-03-27 22:04:25 +0530

    Sample video

--- init-mongo.js
+++ init-mongo.js
@@ -1,8 +1,8 @@
 db = db.getSiblingDB("netflix");  // Use or create the database
 
 db.videos.insertMany([
-    { "title" : "file_example_MP4_640_3MG", "filename" : "file_example_MP4_640_3MG.mp4" },
-    { "title" : "file_example_MP4_640_3MG_copy", "filename" : "file_example_MP4_640_3MG_copy.mp4" }
+    { "title" : "My Sample Video", "filename" : "sample_video_1.mp4" },
+    { "title" : "My Sample Video 2", "filename" : "sample_video_2.mp4" }
 ]);
 
 print("✅ Initial data added to MongoDB!");
